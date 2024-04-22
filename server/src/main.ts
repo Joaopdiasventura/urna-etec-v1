6c683ae35e761dfdcf3e5f695ca716f9a79b6671
João Paulo Dias <joaopdiasventura@gmail.com>
2024-04-22 09:42:31 -0300

    fix: fazendo o nest escutar a porta local

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const port = process.env.PORT;
+
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+	
+	const corsOptions = {
+		origin: [process.env.FRONTEND],
+		methods: ["GET", "DELETE", "POST", "PUT"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	  };
+	
+	app.enableCors(corsOptions);
+
+	await app.listen(port, "0.0.0.0");
 }
+
 bootstrap();
